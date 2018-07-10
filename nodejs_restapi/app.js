b03e14c416859be79983bbf86640b544272dbcd1
baskar44 <baskg99@gmail.com>
2018-07-10 13:13:47 +1000

    Server is connected

---
+++ nodejs_restapi/app.js
@@ -0,0 +1,14 @@
+// Load our app server using express
+
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+  console.log("Responding to root route")
+  res.send("Hello from root.")
+})
+
+// localhost:3003
+app.listen(3003, () => {
+  console.log("Server is up and listening on 3003...")
+})
