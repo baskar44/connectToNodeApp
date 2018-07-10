27db797bb176ca7b505ed926515c9f4b5f30d6db
baskar44 <baskg99@gmail.com>
2018-07-10 19:39:21 +1000

    connected to mysql on localhost

--- nodejs_restapi/app.js
+++ nodejs_restapi/app.js
@@ -3,8 +3,44 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mysql = require('mysql')
 
 app.use(morgan('short'))
+
+app.get('/users/:id', (req, res) => {
+  console.log("Fetching user with id: " + req.params.id)
+
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'connectToNodeApp'
+  })
+
+  const queryString = "SELECT * FROM user WHERE id = ?"
+
+  connection.query(queryString, [req.params.id], (err, rows, fields) => {
+    if (err) {
+      console.log("Failed to query for users: " + err)
+      res.sendStatus(500)
+      res.end()
+      return
+    }
+    console.log("I think we fetched users successfully")
+
+    const users = rows.map((row) => {
+      return {
+        firstName: row.firstName,
+        lastName: row.lastName
+      }
+    })
+
+    res.json(users)
+
+    
+  })
+})
+
+
 
 app.get("/", (req, res) => {
   console.log("Responding to root route")
@@ -12,14 +48,27 @@ app.get("/", (req, res) => {
 })
 
 app.get("/users", (req, res) => {
-  const dom = {firstName: "Dom", lastName: "Cobb"}
-  const joseph = {firstName: "Joseph", lastName: "Gordon-Levitt"}
-  const ellen = {firstName: "Ellen", lastName: "Page"}
-  res.json([dom, joseph, ellen])
-  //res.send("Nodemon auto updates when I save this file")
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'connectToNodeApp'
+  })
+
+  connection.query("SELECT * FROM USER", (err, rows, fields) => {
+    if (err) {
+      console.log("Failed to query for users: " + err)
+      res.sendStatus(500)
+      res.end()
+      return
+    }
+
+    console.log("I think we fetched users successfully")
+    res.json(rows)
+  })
 })
 
 // localhost:3003
 app.listen(3003, () => {
   console.log("Server is up and listening on 3003...")
 })
+
